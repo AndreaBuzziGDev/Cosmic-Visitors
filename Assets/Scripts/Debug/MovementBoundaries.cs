54e4d06d37aabbaac88cb0ef836805401c4beff9
Andrea Buzzi <aby.development@outlook.it>
2023-06-09 14:09:00 +0200

    Getting ready for another micro improvement

---
+++ Assets/Scripts/Debug/MovementBoundaries.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBoundaries : MonoBehaviour
+{
+    //TODO: USE https://forum.unity.com/threads/how-to-get-top-right-bottom-right-corner-of-boxcollider2d.653182/ AS A REFERENCE TO DRAW THE MOVEMENT BOUNDARIES.
+
+}
