d420cd17092f483f80771f80bed2041f6d906ffe
Andrea Buzzi <aby.development@outlook.it>
2023-06-14 00:10:35 +0200

    Introduced Gizmo Boundaries for Player Ship

---
+++ Assets/Scripts/Debug/PlayerShipBoundaries.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipBoundaries : MonoBehaviour
+{
+    //DATA
+    [SerializeField] float Start = -11;
+    [SerializeField] float HorizontalLength = 2;
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(Start, 4, 0), new Vector3(Start + HorizontalLength, 4, 0));
+        Gizmos.DrawLine(new Vector3(Start, -4, 0), new Vector3(Start + HorizontalLength, -4, 0));
+
+#endif
+    }
+
+}
